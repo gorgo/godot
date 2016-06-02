2946fbc10d21db2bf1b362a648b37102d7d5e5a8
gorgo <gorgo_niger@mail.ru>
2016-06-02 16:15:55 +0900

    ultimate sorting stuff, in googlee play

--- platform/android/java/src/org/godotengine/godot/SplashActivity.java
+++ platform/android/java/src/org/godotengine/godot/SplashActivity.java
@@ -3,15 +3,25 @@ package org.godotengine.godot;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+//import com.godot.game.R;
+
+// import android.widget.Button;
+// import android.view.View;
+// import android.view.View.OnClickListener;
+
+import com.appodeal.ads.Appodeal;
 
 public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //setContentView(R.layout.main);
         Intent intent = new Intent(this, org.godotengine.godot.Godot.class);
         startActivity(intent);
         finish();
+
     }
+
+
 }
